09deec2b0c44baf84d71250935e649f406f4f989
Andreas Motl <andreas.motl@elmyra.de>
2020-12-10 00:13:18 +0100

    Make AssemblyScript happy by providing an "abort()" host function

--- kernel/core.py
+++ kernel/core.py
@@ -1,6 +1,8 @@
-# Derived from https://github.com/wasmerio/wasmer-python/blob/master/examples/engine_jit.py
+# Derived from
+# https://github.com/wasmerio/wasmer-python/blob/1.0.0-beta1/examples/engine_jit.py
+# https://github.com/wasmerio/wasmer-python/blob/1.0.0-beta1/examples/imports_function.py
 import sys
-from wasmer import engine, wat2wasm, Store, Module, Instance
+from wasmer import engine, wat2wasm, Store, Module, Instance, ImportObject, Function
 from wasmer_compiler_cranelift import Compiler
 
 
@@ -26,6 +28,38 @@ def load_code(filename):
     return wasm_bytes
 
 
+def host_functions(store: Store) -> ImportObject:
+    """
+    Provide an "abort()" host function.
+    https://github.com/flow-heater/pytshello/issues/1
+
+    :param store:
+    :return:
+    """
+
+    # When creating an `Instance`, we can pass an `ImportObject`. All
+    # entities that must be imported are registered inside the
+    # `ImportObject`.
+    import_object = ImportObject()
+
+    # Let's write the Python function that is going to be imported,
+    # i.e. called by the WebAssembly module.
+    def abort(a: 'i32', b: 'i32', c: 'i32', d: 'i32'):
+        pass
+
+    abort_host_function = Function(store, abort)
+
+    # Now let's register the `sum` import inside the `env` namespace.
+    import_object.register(
+        "env",
+        {
+            "abort": abort_host_function,
+        }
+    )
+
+    return import_object
+
+
 def run_code(wasm_code):
 
     # Define the engine that will drive everything.
@@ -45,11 +79,14 @@ def run_code(wasm_code):
     # the engine.
     module = Module(store, wasm_code)
 
+    # Provide an "abort()" host function.
+    import_object = host_functions(store)
+
     # Congrats, the Wasm module is compiled! Now let's execute it for the
     # sake of having a complete example.
     #
     # Let's instantiate the Wasm module.
-    instance = Instance(module)
+    instance = Instance(module, import_object)
 
     # The Wasm module exports a function called `sum`.
     add = instance.exports.add
@@ -64,7 +101,6 @@ def main():
         code = get_code()
     elif what == "typescript":
         code = load_code("userspace-typescript/build/untouched.wat")
-        raise NotImplementedError("YMMV")
     else:
         raise ValueError("Undefined invocation")
     run_code(code)
